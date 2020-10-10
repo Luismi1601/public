8076be7133e5a33ed64e1f16d13673fc40ab8b85
Luismi1601 <Luismi16@gmail.com>
2020-10-10 19:36:51 +0200

    practica de letras

---
+++ Desafio1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Letras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Letra_E();
+            Letra_P();
+            Letra_P1();
+        }
+
+        static void Letra_E()
+        {
+            char[,] E = new char[7, 7] { {'*','*','*','*','*','*','*'}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*','*','*','*','*',' ',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*','*','*','*','*','*','*'}};
+            int length = 7;
+
+            for (int horizontal = 0; horizontal < length; horizontal++) 
+            {
+                for (int vertical = 0; vertical < length; vertical++)
+                {
+                    Console.Write(E[horizontal,vertical]);
+                }   
+                Console.WriteLine();
+            }
+        }
+
+        static void Letra_P()
+        {
+            char[,] E = new char[7, 7] { {'*','*','*','*','*','*',' '}
+                                       , {'*',' ',' ',' ',' ',' ','*'}
+                                       , {'*',' ',' ',' ',' ',' ','*'}
+                                       , {'*','*','*','*','*','*',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}
+                                       , {'*',' ',' ',' ',' ',' ',' '}};
+            int length = 7;
+
+            for (int horizontal = 0; horizontal < length; horizontal++) 
+            {
+                for (int vertical = 0; vertical < length; vertical++)
+                {
+                    Console.Write(E[horizontal,vertical]);
+                }   
+                Console.WriteLine();
+            }
+        }
+    
+        static void Letra_P1()
+        {
+            char[,] P = new char[7, 7]; 
+            int length = 7;
+            char asterisco='*';
+            char vacio=' ';
+            int horizontal=default;
+            int vertical=default;
+
+            for (horizontal = 0; horizontal < length; horizontal++) 
+            {
+                for (vertical = 0; vertical < length; vertical++)
+                {
+                    P[horizontal,vertical]=(
+                        (vertical==0 || 
+                        (horizontal==0 && vertical<6) ||
+                        (vertical==6 && (horizontal==1 || horizontal==2)) ||
+                        (horizontal==3 && vertical<6)
+                        ? asterisco : vacio));
+
+                    Console.Write(P[horizontal,vertical]);
+                }   
+                Console.WriteLine();
+            }
+        }
+    }
+}
